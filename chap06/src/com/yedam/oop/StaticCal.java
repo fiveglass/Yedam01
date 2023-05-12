7969bec473c9db33a590d4f3d5f4d3574b19ba64
admin <admin@YD02-05>
2023-05-12 10:19:32 +0900

    11

--- chap06/src/com/yedam/oop/StaticCal.java
+++ chap06/src/com/yedam/oop/StaticCal.java
@@ -1,5 +1,17 @@
 package com.yedam.oop;
 
 public class StaticCal {
+	//정적 필드
+	static double pi = 3.14;
+	
+	//정적 메소드
+	static int plus(int x, int y) {
+		return x+y;
+	}
+	
+	//정적 메소드
+	static int minus(int x, int y) {
+		return x-y;
+	}
 
 }
